c1fd15efa3aed275cee61c1004dd427af6bdf27f
Yukaru <57414313+Yukaru-san@users.noreply.github.com>
2021-10-08 16:37:12 +0200

    add img scaling

--- Messages.java
+++ Messages.java
@@ -1,12 +1,14 @@
 
 public class Messages {
 
-	public static final String HELP_MESSAGE = "\nCommands available:\n - search [search term]\n - position [x/reset] [y/reset]\n - window [show/hide]\n - exit";
+	public static final String HELP_MESSAGE = "\nCommands available:\n - search [search term]\n - position [x/reset] [y/reset]\n - scale [x] [y]\n - window [show/hide]\n - exit";
 	
-	public static final String WINDOW_CMD_HELP = "--> Useage: window [show/close]";
-	public static final String SEARCH_CMD_HELP = "--> Useage: search [term]";
-	public static final String POS_CMD_HELP = "--> Useage: position [x] [y] (or use [reset] instead of a number)";
+	public static final String WINDOW_CMD_HELP = "--> Usage: window [show/close]";
+	public static final String SEARCH_CMD_HELP = "--> Usage: search [term]";
+	public static final String POS_CMD_HELP = "--> Usage: position [x] [y] (or use [reset] instead of a number)";
+	public static final String SCALE_CMD_HELP = "--> Usage: scale [x] [y]";
 	
-	
+	public static final String SCALE_BELOW_ZERO_MESSAGE = "--> Scale cannot be lower than 0!";
+	public static final String NAN_MESSAGE = "--> Input was not a number.";
 	public static final String NO_RESULTS_MESSAGE = "--> No results available for your input";
 }
